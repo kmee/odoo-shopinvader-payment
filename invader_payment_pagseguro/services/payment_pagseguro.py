ad5c450be42ef293b00dc4dbe84a0d7a8c19079f
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-05-24 18:02:55 -0300

    [IMP] invader_pagseguro: GET public key route

--- invader_payment_pagseguro/services/payment_pagseguro.py
+++ invader_payment_pagseguro/services/payment_pagseguro.py
@@ -1,16 +1,15 @@
 #  Copyright 2022 KMEE
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import json
 import logging
+
+import requests
 
 from odoo.addons.base_rest import restapi
 from odoo.addons.component.core import AbstractComponent
 
 _logger = logging.getLogger(__name__)
-
-SANDBOX_URL = (
-    "https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code="
-)
 
 
 class PaymentServicePagseguro(AbstractComponent):
@@ -24,28 +23,60 @@ class PaymentServicePagseguro(AbstractComponent):
         return self.component(usage="invader.payment")
 
     @restapi.method(
-        [(["/<int:sale_order_id>"], "GET")],
-        output_param=restapi.CerberusValidator("_return_confirm_payment"),
+        [(["/public-key"], "GET")],
+        input_param=restapi.CerberusValidator("_get_schema_public_key"),
+        output_param=restapi.CerberusValidator(
+            "_get_schema_return_public_key"
+        ),
     )
-    def checkout_url(self, target, **params):
-        """ Get Pagseguro redirect checkout url from current cart """
-        sale_order_id = params.get("sale_order_id")
-        _logger.warning("Sale Order id: ", sale_order_id)
-        code = sale_order_id
-        url = SANDBOX_URL + code
-        return {
-            "checkout_url": url,
-            "success": True,
-            "error": "none",
-        }
+    def public_key(self, target, **params):
+        """ Get public key to encrypt card
 
-    def _return_confirm_payment(self):
+        return public_key (string): public key related to acquirer token
+        """
+        acquirer_id = self.env.ref(
+            "payment_pagseguro.payment_acquirer_pagseguro"
+        ).id
+        payload = {"jsonrpc": "2.0", "params": {"acquirer_id": acquirer_id}}
+        base_url = (
+            self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+        )
+        res = requests.get(
+            "http://" + base_url + ":8069/payment/pagseguro/public_key",
+            json=payload,
+        )
+
+        content = json.loads(res.content)
+        if content.get("result"):
+            return {
+                "public_key": content.get("result"),
+                "success": True,
+            }
+        elif content.get("error"):
+            message = content.get("error", {}).get("data", {}).get("message")
+            return {
+                "public_key": "",
+                "success": False,
+                "error": message,
+            }
+        else:
+            return {
+                "public_key": "",
+                "success": False,
+                "error": "Unknown error",
+            }
+
+    def _get_schema_public_key(self):
+        """ Get default api key and user email validator """
+        return self.payment_service._invader_get_target_validator()
+
+    def _get_schema_return_public_key(self):
         """
         Return validator of checkout_url service
         checkout_url (str): Redirect checkout url
         """
         return {
-            "checkout_url": {"type": "string"},
-            "success": {"type": "boolean"},
-            "error": {"type": "string"},
+            "public_key": {"type": "string", "required": True},
+            "success": {"type": "boolean", "required": True},
+            "error": {"type": "string", "required": False},
         }
